ef2df6997fab360d9db035db86f3d71da3536945
Andrew Chen <91634279+atillathec@users.noreply.github.com>
2022-09-06 11:58:16 -0400

    tableau prep

---
+++ fragments/labels/tableauprep.sh
@@ -0,0 +1,7 @@
+tableauprep)
+    name="Tableau Prep"
+    type="pkgInDmg"
+    packageID="com.tableausoftware.tableauprep"
+    downloadURL="https://www.tableau.com/downloads/prep/mac"
+    expectedTeamID="QJ4XPRK37C"
+    ;;
